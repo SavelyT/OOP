8294e5bc118956be81b2ced7ca710dad21d23a5a
SavelyT <151632890+SavelyT@users.noreply.github.com>
2024-11-20 13:54:30 +0700

    test code

---
+++ ConsoleApplication8/ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,14 @@
+﻿#include <string.h>
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char word[] = "азбука";
+	int count = 0;
+	for (int i = 0; i < strlen(word); i++)
+		if (word[i] == 'а') count++;
+	printf("Столько раз символ 'а' встречается в слове %s: %d", word, count);
+	
+}
+	
